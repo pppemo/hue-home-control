27381008c843a7878aae8906d0fb6dda22b5bf5d
Przemek Krupiński <pkrupinski@worldremit.com>
2020-05-15 16:57:41 +0200

    Added "click" confirmation sound

--- src/components/Switch/index.js
+++ src/components/Switch/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import LongPress from "react-long";
 import { ButtonBase } from "@material-ui/core";
+import Sound from "react-sound";
 import cx from "classnames";
 import styles from "./Switch.module.scss";
 
@@ -15,38 +16,55 @@ const Switch = ({
   onLongPress,
 }) => {
   const [isOn, setIsOn] = useState(isOnProp);
+  const [isConfirmationSoundPlaying, setIsConfirmationSoundPlaying] = useState(
+    false
+  );
 
-  const handleToggle = () => onPress(!isOn);
+  const handleToggle = () => {
+    setIsConfirmationSoundPlaying(true);
+    onPress(!isOn);
+  };
 
   useEffect(() => {
     setIsOn(isOnProp);
   }, [isOnProp]);
 
   return (
-    <LongPress
-      time={1000}
-      onLongPress={!isStateless && onLongPress}
-      onPress={!isDisabled ? handleToggle : undefined}
-    >
-      <div className={styles.container}>
-        <ButtonBase
-          className={cx(styles.switch, {
-            [styles.isActive]: !isDisabled && !isStateless && isOn,
-            [styles.isDisabled]: !isStateless && isDisabled,
-            [styles.isStateless]: isStateless,
-          })}
-        >
-          <div className={styles.label}>
-            {isFavourite && (
-              <span className={styles.favourite} role="img" aria-label="*">
-                ⭐️
-              </span>
-            )}
-            {lightName}
-          </div>
-        </ButtonBase>
-      </div>
-    </LongPress>
+    <>
+      <Sound
+        url="/sounds/click-sound.mp3"
+        playStatus={
+          isConfirmationSoundPlaying
+            ? Sound.status.PLAYING
+            : Sound.status.STOPPED
+        }
+        onFinishedPlaying={() => setIsConfirmationSoundPlaying(false)}
+      />
+      <LongPress
+        time={1000}
+        onLongPress={!isStateless && onLongPress}
+        onPress={!isDisabled ? handleToggle : undefined}
+      >
+        <div className={styles.container}>
+          <ButtonBase
+            className={cx(styles.switch, {
+              [styles.isActive]: !isDisabled && !isStateless && isOn,
+              [styles.isDisabled]: !isStateless && isDisabled,
+              [styles.isStateless]: isStateless,
+            })}
+          >
+            <div className={styles.label}>
+              {isFavourite && (
+                <span className={styles.favourite} role="img" aria-label="*">
+                  ⭐️
+                </span>
+              )}
+              {lightName}
+            </div>
+          </ButtonBase>
+        </div>
+      </LongPress>
+    </>
   );
 };
 
