eb1bf5ee6a2f920a04687fe3b0e6980cd5221a2c
Przemek Krupiński <pkrupinski@worldremit.com>
2020-08-19 13:56:39 +0200

    Built new CRA with typescript from scratch

--- src/components/Spinner/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import BounceLoader from "react-spinners/BounceLoader";
-
-export default ({ isLoading }) => (
-  <BounceLoader sizeUnit={"px"} size={250} color={"#123abc"} loading={isLoading} />
-);
--- src/store/selectors/scenes.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-
-const scenesSelector = state => state.scenes;
-
-export const hasScenes = createSelector(
-  [scenesSelector],
-  scenes => scenes && !!(Object.values(scenes).length > 0)
-);
